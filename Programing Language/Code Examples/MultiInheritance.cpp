ba358e4fa1ee651edf3379b6db8fd05eaad302d8
Steve Lau <liuwenhong_steve@163.com>
2017-10-31 11:07:29 +0800

    Create MultiInheritance.cpp

---
+++ Programing Language/Code Examples/MultiInheritance.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+
+class Grandam
+{
+public:
+     void introduce_self()
+    {
+        cout << "I am grandam." << endl;
+    }
+};
+
+class Mother :virtual public Grandam
+{
+public:
+    void introduce_self()
+    {
+        cout << "I am mother." << endl;
+    }
+};
+
+class Aunt :virtual public Grandam
+{
+public:
+    void introduce_self()
+    {
+        cout << "I am aunt." << endl;
+    }
+};
+
+class Daughter :public Mother,public Aunt
+{
+public:
+    void introduce_self()
+    {
+        cout << "I am daughter." << endl;
+    }
+};
+
+int main()
+{
+    Grandam* ptr;
+    Daughter d;
+
+    ptr = &d;
+
+    ptr->introduce_self();
+
+	cout << sizeof(Grandam) << endl;  // 1
+
+	cout << sizeof(Mother) << endl;   // 4
+
+	cout << sizeof(Aunt) << endl;     // 4
+
+	cout << sizeof(Daughter) << endl; // 8
+
+    return 0;
+}
